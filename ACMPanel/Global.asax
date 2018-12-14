bbfc9569b7948b347c94ad6dc722b970fb506274
Alexander Paulino <alexanderpauul@hotmail.com>
2018-12-14 00:05:39 -0400

    Create Solution Project
    Adding new blanck solution and MVC project template.

---
+++ ACMPanel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ACMPanel.MvcApplication" Language="C#" %>
---
+++ ACMPanel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ACMPanel.Startup))]
+namespace ACMPanel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
